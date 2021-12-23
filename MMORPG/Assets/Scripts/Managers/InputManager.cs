f4f01026e52fe7b22baaf95f17313f4a06a86196
oyh6415 <dudgus6324@naver.com>
2021-12-23 14:50:47 +0900

    .

---
+++ MMORPG/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputManager : MonoBehaviour
+{
+    public Action KeyAction = null;
+
+    public void OnUpdate()
+    {
+        if (Input.anyKey == false)
+            return;
+
+        if (KeyAction != null)
+            KeyAction.Invoke();
+    }
+
+    //이렇게 해 놓으면 KeyAction을 실행하는 것은 한 곳이라 인풋 오류가 났을 때
+    // 16줄에 브레이크를 걸고 안에 무슨 함수가 들었는지 확인할 수 있음
+}
---
+++ MMORPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10.0f;
+
+    private void Start()
+    {
+        Managers.Input.KeyAction -= OnKeyboard;
+        Managers.Input.KeyAction += OnKeyboard;
+
+        //혹시 두 개 들어갈까 봐 제거하고 넣기
+    }
+
+    void OnKeyboard()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
+    }
+}
